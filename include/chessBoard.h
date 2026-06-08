869b79175351d1e1c2a88f64c04e8dc47ed26590
Jamil <jamilpalma704@gmail.com>
2026-06-08 18:27:28 +0800

    added function bodies for getPiece(), setPiece(), and chessBoard()

---
+++ include/chessBoard.h
@@ -0,0 +1,31 @@
+#pragma once
+#include <cstdint>
+
+#define whitePawn = 0;
+#define whiteKing = 1;
+#define whiteQueen = 2;
+#define whiteBishop = 3;
+#define whiteHorse = 4;
+#define whiteRook = 5;
+#define blackPawn = 6;
+#define blackKing = 7;
+#define blackQueen = 8;
+#define blackBishop = 9;
+#define blackHorse = 10;
+#define blackRook = 11;
+#define empty = 12;
+
+class chessBoard{
+public:
+
+    // gets a piece then sets it to a specific square
+    uint8_t getPiece(std::uint8_t square);
+    void setPiecee(std::uint8_t piece, std::uint8_t square);
+
+    chessBoard(); //constructor
+    
+private:
+
+    // stores bitmap for every piece
+    uint64_t chessboards[12];
+};
---
+++ src/chessBoard.cpp
@@ -0,0 +1,44 @@
+#include <chessBoard.h>
+#include <cstdint>
+
+// gets the piece from a selected square
+int8_t chessBoard::getPiece(std::uint8_t square){
+	uint64_t mask = uint64_t(1) << square;
+	
+	for(int i = 0; i < 12; i++){
+		if(bitboard[i] & mask){
+			return 1;
+		}
+	}
+	
+	return EMPTY;
+}
+
+// puts a piece on a square/tile
+void chessBoard::setPiece(uint8_t piece, uint8_t square){
+	uint64_t mask = uint64_t(1) << square;
+	
+	bitboards[piece] |= mask;
+	
+	for(int i = 0; i < 12; i++){
+		if(i != piece){
+			bitboards[i] &= ~mask;
+		}
+	}
+}
+
+Chessboard::Chessboard()
+{
+	bitboards[WHITE_PAWN]   = 0x00ff000000000000;
+	bitboards[WHITE_KNIGHT] = 0x2400000000000000;
+	bitboards[WHITE_BISHOP] = 0x4200000000000000;
+	bitboards[WHITE_ROOK]   = 0x8100000000000000;
+	bitboards[WHITE_QUEEN]  = 0x0800000000000000;
+	bitboards[WHITE_KING]   = 0x1000000000000000;
+	bitboards[BLACK_PAWN]   = 0x000000000000ff00;
+	bitboards[BLACK_KNIGHT] = 0x0000000000000024;
+	bitboards[BLACK_BISHOP] = 0x0000000000000042;
+	bitboards[BLACK_ROOK]   = 0x0000000000000081;
+	bitboards[BLACK_QUEEN]  = 0x0000000000000008;
+	bitboards[BLACK_KING]   = 0x0000000000000010;
+}
